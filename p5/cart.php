3e5ccd9d52f4ce90b1b4918b8bddff5a237fd1d2
Ananda Rizky <anandarizkysetyanu29@gmail.com>
2025-09-29 12:40:03 +0700

    Menambahkan tabel, jumlah, subTotal, dan Total. Lalu menambahkan tombol bayar untuk ke bayar.php

--- p5/cart.php
+++ p5/cart.php
@@ -11,17 +11,51 @@
     <title>Document</title>
 </head>
 <body>
+    <table>
+
+        <tr>
+            <th>PID</th>
+            <th>Nama</th>
+            <th>Harga</th>
+            <th>Jumlah</th>
+            <th>Sub Total</th>
+        </tr>
     <?php
+        $mysqli = new mysqli('localhost', 'root', '', 'nwind');
         $pid=$_GET["pid"];
         $jumlah=$_GET["jumlah"];
-        // var_dump($pid);
-        // var_dump($jumlah);
-        
         $_SESSION["belanja"][$pid]=$jumlah;
-        ///var_dump($_SESSION["belanja"]);
+        $total=0;
         foreach($_SESSION["belanja"] as $pid => $jumlah){
-            print "product id: $pid, jumlah dibeli: $jumlah <br>";
+            $sql="select * from products where ProductID=$pid";
+            $products = $mysqli->query($sql);
+            $p=$products->fetch_object();
+            // print "product id: $pid,$p->ProductName,$p->UnitPrice, jumlah dibeli: $jumlah <br>";
+            $subTotal=$p->UnitPrice*$jumlah;
+            $total=$total+$subTotal;
+            ?>
+            <tr>
+                <td><?= $pid?></td>
+                <td><?= $p->ProductName?></td>
+                <td><?= $p->UnitPrice?></td>
+                <td><?= $jumlah?></td>
+                <td><?= $subTotal?></td>
+            </tr>
+            <?php
         }
+
     ?>
+    <tr>
+        <td></td>
+        <td>Total</td>
+        <td></td>
+        <td></td>
+        <td><?=$total?></td>
+    </tr>
+    </table>
+    <form action="bayar.php" method="GET">
+        <input type="hidden" name="customerID" value="VINET">
+        <button type="submit">Bayar</button>
+    </form>
 </body>
 </html>
